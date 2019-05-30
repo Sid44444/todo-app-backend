5fe383c2e16b19b8b8d8edea73d9bc710040e965
Sarah Adlington <cheshire6655@icloud.com>
2019-05-30 20:56:13 +0100

    Database(RDS) commected to the API

--- tasks.js
+++ tasks.js
@@ -2,17 +2,36 @@ const serverless = require('serverless-http');
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const mysql = require('mysql');
 app.use(cors());
 
-app.get('/tasks', function (request, response) {
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: "tasks"
+});
 
-  const username = request.query.username;
-
-  const someJson = {
-    tasks: ["Completed Javascript challenges",  "Buy gift for friend", "Take the dog for a walk"]
-
-  };
-  response.json(someJson);
-})
+app.get("/tasks", function(request, response) {
+  // const username = request.query.username;
+  let queryToExecute = "SELECT * FROM Tasks";
+  // if (username) {
+  //   query =
+  //     "SELECT * FROM Task JOIN User on Task.UserId = User.UserId WHERE User.Username = " +
+  //     connection.escape(username);
+  // }
+  connection.query(queryToExecute, (err, queryResults) => {
+    if (err) {
+      console.log("Error fetching tasks", err);
+      response.status(500).json({
+        error: err
+      });
+    } else {
+      response.json({
+        tasks: queryResults
+      });
+    }
+  });
+});
 
 module.exports.handler = serverless(app);
